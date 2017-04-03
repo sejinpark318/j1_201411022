f99d8761cde528b0c267f97c9e9530617973393a
박세진 j1_201411022 <sejinpark318@naver.com>
2017-04-03 10:18:40 +0900

    Add files via upload

---
+++ sort/v2/IntegerComparator.java
@@ -0,0 +1,11 @@
+package com.sd.sort.v2;
+import java.util.Date;
+import com.sd.turtle.WeightTurtle;
+import java.util.*;
+
+public class IntegerComparator implements Comparator {
+    public IntegerComparator() {}
+    public int compare(Object o1, Object o2) {
+        return (Integer)o1 - (Integer)o2;
+    }
+}
---
+++ sort/v2/TurtleComparator.java
@@ -0,0 +1,10 @@
+package com.sd.sort.v2;
+import java.util.Date;
+import com.sd.turtle.WeightTurtle;
+import java.util.*;
+
+public class TurtleComparator implements Comparator {
+    public int compare(Object t1, Object t2) {
+        return ((WeightTurtle)t1).getWeight()-((WeightTurtle)t2).getWeight();
+    }
+}
